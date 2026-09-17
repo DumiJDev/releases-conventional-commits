4ed48b423e2826be730b880b4001b539d59df231
Dumilde Fernando <dumilde.fernando@selenium.ao>
2026-09-17 15:56:00 +0100

    feat: add more resources

--- src/main/java/io/github/dumijdev/release_convetional_commits/ReleaseConvetionalCommitsApplication.java
+++ src/main/java/io/github/dumijdev/release_convetional_commits/ReleaseConvetionalCommitsApplication.java
@@ -2,19 +2,12 @@ package io.github.dumijdev.release_convetional_commits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class ReleaseConvetionalCommitsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReleaseConvetionalCommitsApplication.class, args);
-	}
-
-
-	@GetMapping("/hello")
-	public String hello() {
-		return "Welcome to Release Conventional Commits Application!";
 	}
 
 }
